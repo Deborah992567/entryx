9612f46ea7616c687843903e0886210f5b9ca5de
DeborahIwuh <Uchechiiwuh12@icloud.com>
2026-08-03 13:15:17 +0100

    frontend: provide ChartStore at app root

--- frontend/lib/app/entryx_app.dart
+++ frontend/lib/app/entryx_app.dart
@@ -9,6 +9,7 @@ import '../core/token_store.dart';
 import '../core/ws_client.dart';
 import '../features/auth/auth_screen.dart';
 import '../features/auth/auth_store.dart';
+import '../features/chart/chart_store.dart';
 import '../features/market/market_watch_store.dart';
 import '../features/shell/entryx_shell.dart';
 import '../features/shell/system_status_store.dart';
@@ -30,6 +31,7 @@ class _EntryXAppState extends State<EntryXApp> {
   late final SystemStatusStore _status;
   late final WorkspaceStore _workspace;
   late final MarketWatchStore _marketWatch;
+  late final ChartStore _chart;
 
   @override
   void initState() {
@@ -41,11 +43,13 @@ class _EntryXAppState extends State<EntryXApp> {
     _status = SystemStatusStore(api: _api, ws: _ws);
     _workspace = WorkspaceStore(api: _api);
     _marketWatch = MarketWatchStore(api: _api, ws: _ws);
+    _chart = ChartStore(api: _api, ws: _ws);
     _auth.restore();
   }
 
   @override
   void dispose() {
+    _chart.dispose();
     _marketWatch.dispose();
     _workspace.dispose();
     _status.dispose();
@@ -65,6 +69,7 @@ class _EntryXAppState extends State<EntryXApp> {
           Provider<ApiClient>.value(value: _api),
           ChangeNotifierProvider.value(value: _auth),
           ChangeNotifierProvider.value(value: _marketWatch),
+          ChangeNotifierProvider.value(value: _chart),
         ],
         child: ListenableBuilder(
           listenable: _auth,
